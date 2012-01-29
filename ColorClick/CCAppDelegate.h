ffa64c3260a49c53cce531632df734203a87a7e4
tnantoka <bornneet@livedoor.com>
2012-01-29 14:10:21 +0900

    Initial Commit

---
+++ ColorClick/CCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CCAppDelegate.h
+//  ColorClick
+//
+//  Created by Tatsuya Tobioka on 12/01/29.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface CCAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ ColorClick/CCAppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  CCAppDelegate.m
+//  ColorClick
+//
+//  Created by Tatsuya Tobioka on 12/01/29.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "CCAppDelegate.h"
+
+@implementation CCAppDelegate
+
+@synthesize window = _window;
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
